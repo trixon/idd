519516cb681ee6289122c4a565a0c013ca042cbb
Patrik Karlsson <patrik@trixon.se>
2016-06-06 12:39:17 +0200

    Enhance image information manager

--- src/main/java/se/trixon/idd/db/manager/ImageInformationManager.java
+++ src/main/java/se/trixon/idd/db/manager/ImageInformationManager.java
@@ -25,8 +25,25 @@ import com.healthmarketscience.sqlbuilder.dbspec.basic.DbConstraint;
  */
 public class ImageInformationManager extends BaseManager {
 
-    public static final String COL_ID = "image_information_id";
+    public static final String COL_COLOR_DEPTH = "color_depth";
+    public static final String COL_COLOR_MODEL = "color_model";
+    public static final String COL_CREATION_DATE = "creation_date";
+    public static final String COL_DIGITIZATION_DATE = "digitization_date";
+    public static final String COL_FORMAT = "format";
+    public static final String COL_HEIGHT = "height";
+    public static final String COL_ORIENTATION = "orientation";
+    public static final String COL_RATING = "rating";
+    public static final String COL_WIDTH = "width";
     public static final String TABLE_NAME = "image_information";
+    private final DbColumn mColorDepth;
+    private final DbColumn mColorModel;
+    private final DbColumn mCreationDate;
+    private final DbColumn mDigitizationDate;
+    private final DbColumn mFormat;
+    private final DbColumn mHeight;
+    private final DbColumn mOrientation;
+    private final DbColumn mRating;
+    private final DbColumn mWidth;
 
     public static ImageInformationManager getInstance() {
         return Holder.INSTANCE;
@@ -35,7 +52,23 @@ public class ImageInformationManager extends BaseManager {
     private ImageInformationManager() {
         mTable = getSchema().addTable(TABLE_NAME);
 
-        mId = mTable.addColumn(COL_ID, "IDENTITY", null);
+        mId = mTable.addColumn(ImageManager.COL_ID, "BIGINT", null);
+        mRating = mTable.addColumn(COL_RATING, "INT", null);
+        mCreationDate = mTable.addColumn(COL_CREATION_DATE, "TIMESTAMP", null);
+        mDigitizationDate = mTable.addColumn(COL_DIGITIZATION_DATE, "TIMESTAMP", null);
+        mOrientation = mTable.addColumn(COL_ORIENTATION, "INT", null);
+        mWidth = mTable.addColumn(COL_WIDTH, "INT", null);
+        mHeight = mTable.addColumn(COL_HEIGHT, "INT", null);
+        mFormat = mTable.addColumn(COL_FORMAT, "VARCHAR", Integer.MAX_VALUE);
+        mColorDepth = mTable.addColumn(COL_COLOR_DEPTH, "INT", null);
+        mColorModel = mTable.addColumn(COL_COLOR_MODEL, "INT", null);
+
+        String indexName;
+        BaseManager manager;
+
+        manager = ImageManager.getInstance();
+        indexName = getIndexName(new DbColumn[]{manager.getId()}, "fkey");
+        mId.references(indexName, manager.getTable().getName(), manager.getId().getName());
     }
 
     @Override
