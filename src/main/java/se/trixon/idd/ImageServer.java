0045acc5dc4f47e8b31b86b4c3d2a9666d625224
Patrik Karlsson <patrik@trixon.se>
2017-08-29 20:19:43 +0200

    Cleanup communication code

--- src/main/java/se/trixon/idd/ImageServer.java
+++ src/main/java/se/trixon/idd/ImageServer.java
@@ -15,7 +15,6 @@
  */
 package se.trixon.idd;
 
-import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,14 +24,13 @@ import java.net.Socket;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.sql.SQLException;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.Timer;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import se.trixon.almond.util.SystemHelper;
 import se.trixon.almond.util.Xlog;
 import se.trixon.idd.db.Db;
@@ -40,25 +38,25 @@ import se.trixon.idd.db.DbCreator;
 import se.trixon.idd.db.FileVisitor;
 import se.trixon.idl.shared.Commands;
 import se.trixon.idl.shared.IddHelper;
-import se.trixon.idl.shared.ImageClientCommander;
-import se.trixon.idl.shared.ImageServerCommander;
-import se.trixon.idl.shared.ImageServerEvent;
 
 /**
  *
  * @author Patrik Karlsson
  */
-class ImageServer implements ImageServerCommander {
+class ImageServer {
+
+    private static final Logger LOGGER = Logger.getLogger(ImageServer.class.getName());
 
     private Set<ClientThread> mClientThreads = new HashSet<>();
     private final Config mConfig = Config.getInstance();
-    private final Set<ImageClientCommander> mImageClientCommanders = Collections.newSetFromMap(new ConcurrentHashMap<>());
+    private boolean mDirectKill;
+    private boolean mKillInitiated;
     private ServerSocket mServerSocket;
+    private boolean mSuccessfulStart;
 
     ImageServer() throws IOException {
         intiListeners();
         startServer();
-        //initTimer();
 
         while (true) {
             try {
@@ -66,109 +64,73 @@ class ImageServer implements ImageServerCommander {
                 ClientThread clientThread = new ClientThread(socket);
                 mClientThreads.add(clientThread);
                 clientThread.start();
-                Xlog.timedOut(String.format("Client connected: %s:%d (%d)",
-                        socket.getLocalAddress(),
-                        socket.getLocalPort(),
-                        socket.getPort()
-                ));
+                clientConnected(socket);
             } catch (IOException e) {
                 Xlog.timedErr(e.getMessage());
             }
         }
     }
 
-    @Override
-    public void execute(String command, String... args) {
-        Executor executor = new Executor(command, args);
-        if (command.equalsIgnoreCase(Commands.UPDATE)) {
-            Thread t = new Thread(() -> {
-                String resultMessage = executor.execute();
-                notifyClientsExecutor(command, resultMessage);
-            });
-
-            t.start();
-        } else {
-            String resultMessage = executor.execute();
-            notifyClientsExecutor(command, resultMessage);
-        }
+    private void clientConnected(Socket socket) {
+        Xlog.timedOut(String.format("Client connected: %s:%d (%d)",
+                socket.getLocalAddress(),
+                socket.getLocalPort(),
+                socket.getPort()
+        ));
     }
 
-    @Override
-    public String getStatus() {
-        return "status";
-    }
-
-    @Override
-    public void registerClientCommander(ImageClientCommander imageClientCommander, String hostname) {
-        Xlog.timedOut("client connected: " + hostname);
-        mImageClientCommanders.add(imageClientCommander);
-    }
-
-    @Override
-    public void removeClientCommander(ImageClientCommander imageClientCommander, String hostname) {
-        if (mImageClientCommanders.contains(imageClientCommander)) {
-            Xlog.timedOut("client disconnected: " + hostname);
-            mImageClientCommanders.remove(imageClientCommander);
-        }
-    }
-
-    @Override
-    public void shutdown() {
-        Xlog.timedOut("shutdown");
-
-        notifyClientsShutdown();
-
-        System.exit(0);
-    }
-
-    private void initTimer() {
-        Timer timer = new Timer(2000, (ActionEvent e) -> {
-//            try {
-            execute("random");
-//            } catch (RemoteException ex) {
-//                Logger.getLogger(ImageServer.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-        });
-        timer.start();
+    private void clientDisconnected(Socket socket) {
+        Xlog.timedOut(String.format("Client disconnected: %s:%d (%d)",
+                socket.getLocalAddress(),
+                socket.getLocalPort(),
+                socket.getPort()
+        ));
     }
 
     private void intiListeners() {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            notifyClientsShutdown();
+            if (mSuccessfulStart && !mKillInitiated) {
+                mDirectKill = true;
+                shutdown();
+            }
+            Xlog.timedOut("shut down!");
         }));
     }
 
-    private void notifyClientsExecutor(String command, String... strings) {
-        mImageClientCommanders.stream().forEach((imageClientCommander) -> {
-            imageClientCommander.notifyExecutor(command, strings);
-        });
-    }
-
-    private void notifyClientsShutdown() {
-        mImageClientCommanders.stream().forEach((imageClientCommander) -> {
-            imageClientCommander.notifyServer(ImageServerEvent.SHUTDOWN);
-        });
-    }
-
     private String sendFile(String path) {
-        String result;
+        String result = null;
 
-        if (mImageClientCommanders.isEmpty()) {
-            result = "no recievers connected - not sending";
-        } else if (path != null) {
-            result = "recievers connected - try sending file";
-            for (ImageClientCommander clientCommander : mImageClientCommanders) {
-                Thread thread = new Thread(() -> {
-////                        clientCommander.sendFile(remoteInputStreamServer.export());
-                });
-
-                thread.start();
-            }
-        } else {
-            result = "No file to send";
-        }
-
+//        if (mImageClientCommanders.isEmpty()) {
+//            result = "no recievers connected - not sending";
+//        } else if (path != null) {
+//            result = "recievers connected - try sending file";
+//            for (ImageClientCommander clientCommander : mImageClientCommanders) {
+//                Thread thread = new Thread(() -> {
+//////                        clientCommander.sendFile(remoteInputStreamServer.export());
+//                });
+//
+//                thread.start();
+//            }
+//        } else {
+//            result = "No file to send";
+//        }
         return result;
+    }
+
+    private synchronized void shutdown() {
+        Xlog.timedOut("shutting down...");
+        mKillInitiated = true;
+        mClientThreads.forEach((clientThread) -> {
+            try {
+                clientThread.kill();
+            } catch (IOException ex) {
+                LOGGER.log(Level.SEVERE, null, ex);
+            }
+        });
+
+        if (!mDirectKill) {
+            IddHelper.exit();
+        }
     }
 
     private void startServer() {
@@ -180,6 +142,7 @@ class ImageServer implements ImageServerCommander {
             mServerSocket = new ServerSocket(port);
             message = String.format("Listening for connections on port %d", port);
             Xlog.timedOut(message);
+            mSuccessfulStart = true;
         } catch (IOException e) {
             Xlog.timedErr(e.getMessage());
             IddHelper.exit();
@@ -188,83 +151,75 @@ class ImageServer implements ImageServerCommander {
 
     class ClientThread extends Thread {
 
-        private String clientName = null;
         private BufferedReader is = null;
         private final Socket mSocket;
         private PrintStream os = null;
 
         public ClientThread(Socket clientSocket) {
+            setName(String.format("%s [%s]", getClass().getCanonicalName(), clientSocket.getInetAddress()));
             mSocket = clientSocket;
         }
 
         @Override
         public void run() {
             try {
-                // Create input and output streams for this client.
                 is = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
                 os = new PrintStream(mSocket.getOutputStream());
-                String name;
-                name = "sd" + 2;
-                while (true) {
-                    os.println("Enter your name.");
-                    name = is.readLine().trim();
-                    if (name.indexOf('@') == -1) {
-                        break;
-                    } else {
-                        os.println("The name should not contain '@' character.");
-                    }
-                }
 
-                /* Welcome the new the client. */
-                os.println("Welcome " + name + " to our chat room.\nTo leave enter /quit in a new line.");
-                synchronized (this) {
-                    for (ClientThread clientThread : mClientThreads) {
-                        if (clientThread == this) {
-                            clientName = "@" + name;
-                        } else {
-                            clientThread.os.println("*** A new user " + name + " entered the chat room !!! ***");
-                        }
-                    }
-                }
+                os.printf("OK IDD %d\n", IddHelper.PROTOCOL_VERSION);
 
-                /* Start the conversation. */
                 while (true) {
                     String line = is.readLine();
                     if (line != null) {
-                        if (line.startsWith("/quit")) {
+                        if (StringUtils.startsWith(line, "close")) {
                             break;
                         }
 
-                        synchronized (this) {
-                            for (ClientThread clientThread : mClientThreads) {
-                                if (clientThread.clientName != null) {
-                                    clientThread.os.println("<" + name + "> " + line);
-                                }
-                            }
+                        if (!StringUtils.isBlank(line)) {
+                            parseCommand(line);
                         }
+
+//                        synchronized (this) {
+//                            for (ClientThread clientThread : mClientThreads) {
+//                                if (clientThread.clientName != null) {
+//                                    clientThread.os.println("<" + name + "> " + line);
+//                                }
+//                            }
+//                        }
                     }
                 }
 
-                synchronized (this) {
-                    for (ClientThread clientThread : mClientThreads) {
-                        if (clientThread != this && clientThread.clientName != null) {
-                            clientThread.os.println("*** The user " + name + " is leaving the chat room !!! ***");
-                        }
-                    }
-                }
-                os.println("*** Bye " + name + " ***");
+                clientDisconnected(mSocket);
 
                 synchronized (this) {
                     mClientThreads.remove(this);
                 }
 
-                // Close the output stream, close the input stream, close the socket.
-                is.close();
-                os.close();
-                mSocket.close();
+                kill();
             } catch (IOException e) {
                 Xlog.timedErr(e.getMessage());
             }
+        }
+
+        private void parseCommand(String command) {
+            Xlog.timedOut("parse: " + command);
+            String[] elements = StringUtils.split(command, " ");
+
+            String cmd = elements[0];
+            String[] args = ArrayUtils.remove(elements, 0);
+
+            if (StringUtils.equals(cmd, "kill")) {
+                shutdown();
+            } else {
+                os.printf("ACK [5@0] {} unknown command \"%s\"\n", cmd);
+            }
+        }
+
+        void kill() throws IOException {
+            os.println("*** Bye");
+            is.close();
+            os.close();
+            mSocket.close();
         }
     }
 
